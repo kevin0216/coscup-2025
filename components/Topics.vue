2ce9e5f3a903c73b3db514afb40758708f34affa
Mirumo <mysteryperson520@gmail.com>
2025-08-03 21:12:54 +0800

    feat: topic page

---
+++ components/Topics.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { data } from '#loaders/community.data'
+import { computed } from 'vue'
+import TopicCard from './TopicCard.vue'
+
+const topics = computed(() => data.communities.reduce((acc, curr) => {
+  const t = curr.topics
+  if (!t) return acc
+  t.image = curr.image
+  acc.push(t)
+
+  return acc
+}, []))
+</script>
+
+<template>
+  <div class="topic-list">
+    <TopicCard
+      v-for="topic in topics"
+      :key="topic.topic_id"
+      :topic="topic"
+    />
+  </div>
+</template>
+
+<style scoped>
+.topic-list {
+  margin-top: 10px;
+}
+
+.topic-card {
+  margin: 20px 0;
+}
+</style>
---
+++ content/en/topics/index.md
@@ -0,0 +1,11 @@
+---
+aside: false
+---
+
+<script setup lang="ts">
+import Topics from '#components/Topics.vue'
+</script>
+
+# Topics and Co-located Events {style="text-align:center"}
+
+<Topics />
